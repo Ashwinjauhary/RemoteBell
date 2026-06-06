e37c810e4069aa3c5786d6b1f8476de51e628c6d
Ashwinjauhary <ashwin2431333@gmail.com>
2026-06-06 15:10:50 +0530

    Update channel to remote_bell_7 for custom sound

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/ring-bell', async (req, res) => {
       android: {
         priority: 'high',
         notification: {
-          channelId: 'remote_bell_6',
+          channelId: 'remote_bell_7',
           sound: 'loud_bell',
           notificationCount: 1,
         },
